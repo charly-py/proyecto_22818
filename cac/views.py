3f95eb4941013e9f3ad17757a4e364b947704424
Carlos Perez <carlosalbetoperez.i@gmail.com>
2022-10-03 19:51:10 -0300

    Actualización clase 11

--- cac/views.py
+++ cac/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
+
+def index(request):
+    if(request.method=='GET'):
+        titulo = 'Titulo cuando accedo por GET'
+    else:
+        titulo = f'Titulo cuando accedo por otro metodo {request.method}'
+    parametros_get = request.GET.get('nombre')
+    return HttpResponse(f"""
+        <h1>{titulo}</h1>
+        <p>{parametros_get}</p>
+    """)
+
+def quienes_somos(request):
+    # return redirect('saludar_solito')
+    return redirect(reverse('saludar', kwargs={'nombre': 'PepaPig'}))
+  
 def hola_mundo(request):
     return HttpResponse('Hola Mundo Django 🦄')
     
@@ -13,5 +30,26 @@ def saludar(request,nombre='Charly'):
 def ver_proyectos(request,anio,mes):
     return HttpResponse(f"""
         <h1>Proyectos del {mes}/{anio}</h1>
+        <p>Listado de proyectos</p>
+    """)
+  
+def cursos_detalle(request, nombre_curso):
+    """"
+    ir a buscar en mi base datos el curso que tenga ese nombre: slug
+    """
+    return HttpResponse(f'{nombre_curso}')
+    
+def cursos(request, nombre_categoria):
+    return HttpResponse(f'{nombre_categoria}')
+  
+def ver_proyectos(request,anio,mes=1):
+    return HttpResponse(f"""
+        <h1>Proyectos del {mes}/{anio}</h1>
+        <p>Listado de proyectos</p>
+    """)
+  
+def ver_proyectos_2022_08(request):
+    return HttpResponse(f"""
+        <h1>Proyectos del mes de agosto del 2022</h1>
         <p>Listado de proyectos</p>
     """)
