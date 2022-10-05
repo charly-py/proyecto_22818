95695f27eb827c8b451e2a414c71579a3723b6af
Carlos Perez <carlosalbetoperez.i@gmail.com>
2022-10-05 20:09:32 -0300

    Actualizacion archivos urls.py y views.py Clase 13

--- cac/views.py
+++ cac/views.py
@@ -9,10 +9,10 @@ from django.template import loader
 
 def index(request):
     if(request.method=='GET'):
-        titulo = 'Titulo cuando accedo por GET'
+        titulo = 'Titulo cuando accedo por GET - MODIFICO EL VALOR'
     else:
         titulo = f'Titulo cuando accedo por otro metodo {request.method}'
-    parametros_get = request.GET.get('nombre')
+    parametetros_get = request.GET.get('nombre')
     listado_cursos = [
         {
             'nombre':'Fullstack Java',
@@ -31,11 +31,11 @@ def index(request):
         },
     ]
     return render(request,'cac/index.html',{'titulo':titulo,'cursos':listado_cursos})
-
     # return HttpResponse(f"""
     #     <h1>{titulo}</h1>
-    #     <p>{parametros_get}</p>
+    #     <p>{parametetros_get}</p>
     # """)
+
 
 def quienes_somos(request):
     #return redirect('saludar_solito')
@@ -43,38 +43,102 @@ def quienes_somos(request):
     template = loader.get_template('cac/quienes_somos.html')
     contexto = {'titulo':'Codo a Codo - Quienes Somos'}
     return HttpResponse(template.render(contexto,request))
-  
-def hola_mundo(request):
-    return HttpResponse('Hola Mundo Django 🦄')
-    
-def saludar(request,nombre='Charly'):
-    return HttpResponse(f"""
-        <h1>Hola mundo Django - Un gusto {nombre}</h1>
-    """)
-  
-def ver_proyectos(request,anio,mes):
-    return HttpResponse(f"""
-        <h1>Proyectos del {mes}/{anio}</h1>
-        <p>Listado de proyectos</p>
-    """)
-  
-def cursos_detalle(request, nombre_curso):
-    """"
-    ir a buscar en mi base datos el curso que tenga ese nombre: slug
-    """
-    return HttpResponse(f'{nombre_curso}')
-    
-def cursos(request, nombre_categoria):
-    return HttpResponse(f'{nombre_categoria}')
-  
-def ver_proyectos(request,anio,mes=1):
-    return HttpResponse(f"""
-        <h1>Proyectos del {mes}/{anio}</h1>
-        <p>Listado de proyectos</p>
-    """)
-  
-def ver_proyectos_2022_08(request):
-    return HttpResponse(f"""
-        <h1>Proyectos del mes de agosto del 2022</h1>
-        <p>Listado de proyectos</p>
-    """)
+
+def ver_proyectos(request,anio=2022,mes=1):
+    proyectos = []
+    return render(request,'cac/proyectos.html',{'proyectos':proyectos})
+
+def ver_cursos(request,anio=2022,mes=1):
+    listado_cursos = [
+        {
+            'nombre':'Fullstack Java',
+            'descripcion': 'Curso curso',
+            'categoria':'Programación'
+        },
+        {
+            'nombre':'Diseño UX/UI',
+            'descripcion': '🎨',
+            'categoria':'Diseño'
+        },
+        {
+            'nombre':'Big Data',
+            'descripcion': 'test test',
+            'categoria':'Analisis de datos'
+        },
+    ]
+    return render(request,'cac/cursos.html',{'cursos':listado_cursos})
+
+
+## Clase 12
+
+##def index(request):
+##    if(request.method=='GET'):
+##        titulo = 'Titulo cuando accedo por GET'
+##    else:
+##        titulo = f'Titulo cuando accedo por otro metodo {request.method}'
+##    parametros_get = request.GET.get('nombre')
+##    listado_cursos = [
+##        {
+##            'nombre':'Fullstack Java',
+##            'descripcion': 'Curso curso',
+##            'categoria':'Programación'
+##        },
+##        {
+##            'nombre':'Diseño UX/UI',
+##            'descripcion': '🎨',
+##            'categoria':'Diseño'
+##        },
+##        {
+##            'nombre':'Big Data',
+##            'descripcion': 'test test',
+##            'categoria':'Analisis de datos'
+##        },
+##    ]
+##    return render(request,'cac/index.html',{'titulo':titulo,'cursos':listado_cursos})
+##
+##    # return HttpResponse(f"""
+##    #     <h1>{titulo}</h1>
+##    #     <p>{parametros_get}</p>
+##    # """)
+##
+##def quienes_somos(request):
+##    #return redirect('saludar_solito')
+##    #return redirect(reverse('saludar', kwargs={'nombre': 'PepaPig'}))
+##    template = loader.get_template('cac/quienes_somos.html')
+##    contexto = {'titulo':'Codo a Codo - Quienes Somos'}
+##    return HttpResponse(template.render(contexto,request))
+##  
+##def hola_mundo(request):
+##    return HttpResponse('Hola Mundo Django 🦄')
+##    
+##def saludar(request,nombre='Charly'):
+##    return HttpResponse(f"""
+##        <h1>Hola mundo Django - Un gusto {nombre}</h1>
+##    """)
+##  
+##def ver_proyectos(request,anio,mes):
+##    return HttpResponse(f"""
+##        <h1>Proyectos del {mes}/{anio}</h1>
+##        <p>Listado de proyectos</p>
+##    """)
+##  
+##def cursos_detalle(request, nombre_curso):
+##    """"
+##    ir a buscar en mi base datos el curso que tenga ese nombre: slug
+##    """
+##    return HttpResponse(f'{nombre_curso}')
+##    
+##def cursos(request, nombre_categoria):
+##    return HttpResponse(f'{nombre_categoria}')
+##  
+##def ver_proyectos(request,anio,mes=1):
+##    return HttpResponse(f"""
+##        <h1>Proyectos del {mes}/{anio}</h1>
+##        <p>Listado de proyectos</p>
+##    """)
+##  
+##def ver_proyectos_2022_08(request):
+##    return HttpResponse(f"""
+##        <h1>Proyectos del mes de agosto del 2022</h1>
+##        <p>Listado de proyectos</p>
+##    """)
